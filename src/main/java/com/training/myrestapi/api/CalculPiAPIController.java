a5f0e6b11a2916cbd4d9560619c6aa405a7fe5da
PeterisAlexander <pad7887@gmail.com>
2022-02-03 17:36:59 +0100

    calcul pi with param

--- src/main/java/com/training/myrestapi/api/CalculPiAPIController.java
+++ src/main/java/com/training/myrestapi/api/CalculPiAPIController.java
@@ -1,5 +1,28 @@
 package com.training.myrestapi.api;
 
-public class CalculPiAPIController {
+import java.math.BigDecimal;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/calculPi")
+public class CalculPiAPIController {
+	@SuppressWarnings("deprecation")
+	@GetMapping(value = "", produces = "application/json")
+	public BigDecimal calculPiToCirconferenceCircle(
+			@RequestParam("diametre") float diametre,
+			@RequestParam("rayon") float rayon,
+			@RequestParam("indice") float indice
+			) {
+		
+		double circonference = Math.PI * diametre;
+		double pi = diametre / circonference;
+		
+		BigDecimal result = (new BigDecimal(pi)).setScale(2, BigDecimal.ROUND_CEILING);
+				
+		return result;
+	}
 }
