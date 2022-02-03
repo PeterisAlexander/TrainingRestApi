3a1f53bd7ce5b7afe017ec0f86ac89fd36ec6c72
PeterisAlexander <pad7887@gmail.com>
2022-02-03 17:49:42 +0100

    add method round

--- src/main/java/com/training/myrestapi/api/CalculPiAPIController.java
+++ src/main/java/com/training/myrestapi/api/CalculPiAPIController.java
@@ -1,7 +1,5 @@
 package com.training.myrestapi.api;
 
-import java.math.BigDecimal;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,19 +8,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/calculPi")
 public class CalculPiAPIController {
-	@SuppressWarnings("deprecation")
+
 	@GetMapping(value = "", produces = "application/json")
-	public BigDecimal calculPiToCirconferenceCircle(
+	public double calculPiToCirconferenceCircle(
 			@RequestParam("diametre") float diametre,
 			@RequestParam("rayon") float rayon,
-			@RequestParam("indice") float indice
+			@RequestParam("indice") int indice
 			) {
 		
 		double circonference = Math.PI * diametre;
 		double pi = diametre / circonference;
 		
-		BigDecimal result = (new BigDecimal(pi)).setScale(2, BigDecimal.ROUND_CEILING);
-				
-		return result;
+		return roundAvoid(pi,indice);
+	}
+	
+	public static double roundAvoid(double value, int places) {
+	    double scale = Math.pow(10, places);
+	    return Math.round(value * scale) / scale;
 	}
 }
